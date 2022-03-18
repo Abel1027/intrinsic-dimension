718cd36a26f37968299991fdbbb4affdb90f1c16
Abel1027 <arodriguezm1027@gmail.com>
2022-03-18 22:31:56 +0100

    [TEST][IntrinsicDimension] - Test intrinsic dimension widget

--- test/intrinsic_dimension_test.dart
+++ test/intrinsic_dimension_test.dart
@@ -1,5 +1,64 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:intrinsic_dimension/intrinsic_dimension.dart';
+
+class IntrinsicDimensionExample extends StatelessWidget {
+  const IntrinsicDimensionExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const double _height = 100;
+
+    return MaterialApp(
+      title: 'Intrinsic Dimension Example',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Intrinsic Dimension Example'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: IntrinsicDimension(
+            builder: (context, width, height, startOffset) {
+              return Row(
+                children: [
+                  if (height == _height)
+                    Container(
+                      key: const Key('Container'),
+                      width: 2,
+                      height: height,
+                      color: Colors.black,
+                    ),
+                  const SizedBox(width: 8),
+                  SizedBox(
+                    height: _height,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text('Vertical line'),
+                        Text('Offset: $startOffset'),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 void main() {
-  test('Test', () {});
+  testWidgets('Find vertical line from container', (tester) async {
+    await tester.pumpWidget(const IntrinsicDimensionExample());
+
+    await tester.pumpAndSettle();
+
+    final containerFinder = find.byKey(const Key('Container'));
+    expect(containerFinder, findsOneWidget);
+  });
 }
